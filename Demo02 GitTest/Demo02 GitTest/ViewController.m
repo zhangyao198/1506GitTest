71eb5561c2de9acaf4b9e7549af5982954c709b5
张藤耀 <lilu@changjiang-2.local>
2015-10-08 13:15:47 +0800

    第一个修改

---
+++ Demo02 GitTest/Demo02 GitTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Demo02 GitTest
+//
+//  Created by 张藤耀 on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"一个");
+    NSLog(@"二个");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
